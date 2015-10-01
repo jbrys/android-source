0a60680574bb87913e0dbbb86317f5ffac8ef2ca
Jeff Brys <jeffreybrys@gmail.com>
2015-10-01 10:29:34 -0700

    Assignment 17 Complete (for reals)

--- foundation/17-loops/src/com/bloc/loops/Loops.java
+++ foundation/17-loops/src/com/bloc/loops/Loops.java
@@ -10,22 +10,18 @@ public class Loops extends Object {
 		boolean[] someBools = {true, false, true, true, false, true, false, false};
 		boolean temp = false;
 
+		
+
 		//********** Starts Here ************************/
-		temp = someBools[7];
-		someBools[7] = someBools[0];
-		someBools[0] = temp;
+		int i = 0;
+		
+		while (i < 4) {
+			temp = someBools[7 - i];
+			someBools[7 - i] = someBools[i];
+			someBools[i] = temp;
 
-		temp = someBools[6];
-		someBools[6] = someBools[1];
-		someBools[1] = temp;
-
-		temp = someBools[5];
-		someBools[5] = someBools[2];
-		someBools[2] = temp;
-
-		temp = someBools[4];
-		someBools[4] = someBools[3];
-		someBools[3] = temp;
+			i++;
+		}
 		//********** Ends Here **************************/
 		
 
@@ -49,15 +45,12 @@ public class Loops extends Object {
 		 * 	Learn more here: http://www.cafeaulait.org/course/week2/43.html
 		/************************************************/
 
+		
+
 		//********** Starts Here ************************/
-		numArray[0] = !someBools[0] ? 1 : 0;
-		numArray[1] = !someBools[1] ? 1 : 0;
-		numArray[2] = !someBools[2] ? 1 : 0;
-		numArray[3] = !someBools[3] ? 1 : 0;
-		numArray[4] = !someBools[4] ? 1 : 0;
-		numArray[5] = !someBools[5] ? 1 : 0;
-		numArray[6] = !someBools[6] ? 1 : 0;
-		numArray[7] = !someBools[7] ? 1 : 0;
+		for (int k = 0; k < 8; k++){
+			numArray[k] = !someBools[k] ? 1 : 0;
+		}
 		//********** Ends Here **************************/
 
 
