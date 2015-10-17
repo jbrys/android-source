22e963070b9e6f689d8aa5cf627a1aee94c5f40e
Jeff Brys <jeffreybrys@gmail.com>
2015-10-17 15:57:15 -0700

    I think this is done, but it throws a weird error at the end

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,5 +1,10 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
 
 	/*
@@ -18,6 +23,13 @@ public class ImageGetter extends Thread {
  	 *	The variables passed into it must impact the `run()` method's
  	 *	behavior as described above.
 	/************************************************/
+	String mURL;
+	boolean mOpenWhenCompleted;
+
+	public ImageGetter(String url, boolean openWhenCompleted) {
+		mURL = url;
+		mOpenWhenCompleted = openWhenCompleted;
+	}
 
 	@Override
 	public void run() {
@@ -26,5 +38,29 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+			
+		try {
+			URL url = new URL(mURL);
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator")) && mOpenWhenCompleted) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
 	}
 }
